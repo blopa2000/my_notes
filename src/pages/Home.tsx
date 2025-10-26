96801a6cda0dbf66f567893b3131392b899fe4e6
blopa2000 <jpagudelo2000@gmail.com>
2025-10-26 14:03:30 -0500

    Functional search

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,41 @@
 import MasonryGrid from "@/components/MasonryGrid";
 import { useNotes } from "@/context/notes/NotesContext";
 import "@/styles/home.css";
+import { useState } from "react";
 import { Link } from "react-router";
 
 export const Home = () => {
-  const notes = useNotes()?.notes ?? [];
+  const { notes } = useNotes() ?? [];
+  const [search, setSearch] = useState(notes);
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(
+      notes.filter((note) => {
+        console.log(note.title.toLowerCase().includes(e.target.value.toLowerCase()));
+
+        return note.title.toLowerCase().includes(e.target.value.toLowerCase());
+      })
+    );
+  };
 
   return (
     <div>
       <div className="actions-container">
         <div className="actions-container-search">
-          <input type="text" placeholder="Titulo de nota..." />
+          <input
+            onChange={handleSearch}
+            name="search"
+            type="text"
+            placeholder="Titulo de nota..."
+          />
         </div>
         <div className="actions-container-add-note">
           <Link to="/dashboard/add-Note">Add Note</Link>
         </div>
       </div>
-      <MasonryGrid notes={notes} />
+      {search.length === 0 && notes.length > 0 && <div>No veo lo que buscas</div>}
+      {notes.length === 0 && <div>No tienes notas, agrega una</div>}
+      <MasonryGrid notes={search} />
     </div>
   );
 };
