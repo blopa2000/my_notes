6def258eb1398c263a03d60f932d27933479807e
blopa2000 <jpagudelo2000@gmail.com>
2025-12-28 11:08:12 -0500

    quill fixed

--- src/components/QuillEditor.tsx
+++ src/components/QuillEditor.tsx
@@ -6,7 +6,6 @@ import { quillModules, quillFormats } from "@/utils/quillModules";
 import "react-quill-new/dist/quill.snow.css";
 import "@/styles/quillEditor.css";
 import "highlight.js/styles/github.css";
-import hljs from "highlight.js";
 
 export function QuillEditor({ value, onChange }: TiptapEditorProps) {
   const quillRef = useRef<ReactQuill | null>(null);
@@ -14,13 +13,6 @@ export function QuillEditor({ value, onChange }: TiptapEditorProps) {
   useEffect(() => {
     registerQuill();
   }, []);
-
-  useEffect(() => {
-    const blocks = document.querySelectorAll(".ql-code-block-container");
-    blocks.forEach((block) => {
-      hljs.highlightElement(block as HTMLElement);
-    });
-  }, [value]);
 
   useEffect(() => {
     const editor = document.querySelector(".ql-editor");
